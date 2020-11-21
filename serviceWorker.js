65b3eaafe6a9c000f02907ffce974a784903108f
ophza <jonathanlet@icloud.com>
2020-11-20 17:10:59 -0700

    Added urls that will be cached out

---
+++ serviceWorker.js
@@ -0,0 +1,16 @@
+const CACHE_NAME = "stored-cache";
+const urlsToCache = [
+   "/",
+   "./public/icons/icon-192x192.png",
+   "./public/icons/icon-512x512.png",
+   "./public/index.html",
+   "./public/index.js",
+   "./public/styles.css",
+   "./routes/api.js",
+   "./models/transaction.js",
+   "./server.js",
+   "./api/transaction/bulk",
+   "./api/transaction",
+   "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+   "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+]
